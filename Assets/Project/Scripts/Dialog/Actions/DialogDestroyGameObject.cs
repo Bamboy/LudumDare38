59afca89b02b6c92d67d08aa09cf75bcca886569
Stephan Ennen <bamboy360@gmail.com>
2017-05-07 14:44:49 -0700

    DialogMultiAction class, file organization

---
+++ Assets/Project/Scripts/Dialog/Actions/DialogDestroyGameObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Dialog/Destroy Child")]
+public class DialogDestroyGameObject : DialogAction
+{
+	public string childName = "";
+	public override void DoAction( DialogSequence caller )
+	{
+
+		List<Transform> children = new List<Transform>();
+		foreach (Transform child in caller.transform) 
+		{
+			if( child.name == childName )
+				children.Add( child );
+		}
+
+		if( children.Count > 0 )
+		{
+			GameObject.Destroy( children[ Random.Range(0, children.Count) ].gameObject );
+		}
+
+	}
+
+}
---
+++ Assets/Project/Scripts/Dialog/Actions/DialogIncrementBlock.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Dialog/Increment Dialog Block")]
+public class DialogIncrementBlock : DialogAction 
+{
+	public override void DoAction( DialogSequence caller )
+	{
+		caller.blockIndex++;
+	}
+}
